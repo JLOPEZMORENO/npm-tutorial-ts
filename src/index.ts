da3314eab272dadc5caae8558a03c2ed8a2485af
Juan Lopez Moreno <juanlopezmoreno@Juans-MacBook-Pro.local>
2024-03-12 08:36:54 +0100

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 export class Log {
     static danger(message:string){
-        console.log()
+        console.log(`%c ${message}`, "color: red")
+    }
+    
+    static succes(message:string){
+        console.log(`%c ${message}`, "color: green")
+    }
+
+    static info(message: string){
+        console.log(`%c ${message}`, "color: black; background: yellow")
     }
 }
